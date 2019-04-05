ce8aa8386d5e40288170aff4becec95a2c9e0958
salma salah mahmoud <40217644+Eng-SalmaSalah@users.noreply.github.com>
2019-04-05 15:31:04 +0200

    share photos in my app

--- app/src/main/java/com/salma/downloadimageservice/ImageActivity.java
+++ app/src/main/java/com/salma/downloadimageservice/ImageActivity.java
@@ -3,34 +3,61 @@ package com.salma.downloadimageservice;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 public class ImageActivity extends AppCompatActivity {
-ImageView flagImage;
+    ImageView flagImage;
+    String action;
+    String type;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image);
-        Intent intent=getIntent();
-        String imagePath = intent.getStringExtra("imageName");
-        flagImage=findViewById(R.id.imgFlag);
-        Bitmap bitmap=null;
-        try {
-           // read image from file
-            bitmap = BitmapFactory.decodeStream(openFileInput(imagePath));
-            flagImage.setImageBitmap(bitmap);
+        flagImage = findViewById(R.id.imgFlag);
+        Intent intent = getIntent();
+        //to allow sharing photos in my app
+        action = intent.getAction();
+        type = intent.getType();
+
+        if (Intent.ACTION_SEND.equals(action) && type != null) {
+            if (type.startsWith("image/")) {
+                handleReceivedImage(intent);
+            }
         }
 
-        catch (FileNotFoundException e) {
-            e.printStackTrace();
+        //end of sharing photos in my app
+        else {
+            String imagePath = intent.getStringExtra("imageName");
+            Bitmap bitmap = null;
+            try {
+                // read image from file
+                bitmap = BitmapFactory.decodeStream(openFileInput(imagePath));
+                flagImage.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
         }
+    }
 
+
+    //to allow sharing photos in my app
+    private void handleReceivedImage(Intent intent) {
+        Uri imageUri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
+        if (imageUri != null) {
+            flagImage.setImageURI(imageUri);
+
+        } else {
+            Toast.makeText(this, "Error, URI is invalid", Toast.LENGTH_LONG).show();
+        }
     }
 }
